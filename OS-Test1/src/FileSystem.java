7e5b2e43b674d725675b93035479c9d5ffe7c777
mjuclass <mjuclass@users.noreply.github.com>
2022-11-09 15:44:56 +0900

    1

---
+++ OS-Test1/src/FileSystem.java
@@ -0,0 +1,19 @@
+public class FileSystem extends Thread {
+
+	private Queue<Interrupt> interruptQueue;
+	
+	public FileSystem(Queue<Interrupt> interruptQueue) {
+		this.interruptQueue = interruptQueue;
+	}
+	
+	public void initialize() {
+	}
+	public void finish() {
+	}
+
+	
+	public void run() {
+		
+	}
+
+}
---
+++ OS-Test1/src/Process.java
@@ -0,0 +1,160 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Process {
+	// CPU
+	private static final int MAX_REGISTERS = 10;
+	private int PC;
+	private int codeSize, dataSize, stackSize, heapSize;
+	private Vector<Integer> registers;
+	
+	// Memory
+	private Vector<Instruction> codeList;
+	private Vector<Integer> dataSegment;
+	private Vector<Integer> stackSegment;
+	private Vector<Integer> heapSegment;
+	
+	// Parser
+	private Map<String, String> labelMap;
+	 
+	public Process() {
+		this.registers = new Vector<Integer>();
+		for (int i=0; i<MAX_REGISTERS; i++) {
+			this.registers.add(i);
+		}
+		
+		this.codeList = new Vector<Instruction>();
+		this.dataSegment = new Vector<Integer>();
+		this.stackSegment = new Vector<Integer>();
+		this.heapSegment = new Vector<Integer>();
+		
+		this.labelMap = new HashMap<String, String>();
+	}
+	public void initialize() {
+	}
+	public void finish() {
+	}
+	
+	
+	private void parseData (Scanner scanner) {
+		String command = scanner.next();
+		while (command.compareTo(".end") != 0) {
+			String operand = scanner.next();
+			int size = Integer.parseInt(operand);
+			if (command.compareTo("codeSize") == 0) {
+				this.codeSize = size;
+			} else if (command.compareTo("dataSize") == 0) {
+				this.dataSize = size;
+				for (int i=0; i<dataSize; i++) {
+					this.dataSegment.add(i);
+				}
+			} else if (command.compareTo("stackSize") == 0) {
+				this.stackSize = size;
+				for (int i=0; i<stackSize; i++) {
+					this.stackSegment.add(i);
+				}
+			} else if (command.compareTo("heapSize") == 0) {
+				this.heapSize = size;
+				for (int i=0; i<heapSize; i++) {
+					this.heapSegment.add(i);
+				}
+			}
+			command = scanner.next();
+		}		
+	}
+	
+	private void parsePhaseI(Scanner scanner) {
+		String line = scanner.nextLine();
+		while (scanner.hasNext()) {
+			Instruction instruction = new Instruction(line);
+			if (instruction.getCommand().compareTo("label") == 0) {
+				this.labelMap.put(
+					instruction.getOperand1(), 
+					Integer.toString(this.codeList.size())
+				);
+			} else if (instruction.getCommand().compareTo("") == 0) {				
+			} else if (instruction.getCommand().compareTo("//") == 0) {
+			} else {
+				this.codeList.add(instruction);
+			}
+			line = scanner.nextLine();
+		}
+	}
+	
+	private void parsePhaseII() {
+		for (Instruction instruction: this.codeList) {
+			if ((instruction.getCommand().compareTo("jump")==0) ||
+				(instruction.getCommand().compareTo("greaterThanEqual")==0)
+			   ) {
+				instruction.setOperand1(this.labelMap.get(instruction.getOperand1()));
+			}
+		}
+	}
+	
+	private void parseCode(Scanner scanner) {
+		this.parsePhaseI(scanner);
+		this.parsePhaseII();
+	}
+	
+	public void parse(Scanner scanner) {
+		while (scanner.hasNext()) {
+			String token = scanner.next();
+			if (token.compareTo(".program") == 0) {				
+			} else if (token.compareTo(".code") == 0) {
+				this.parseCode(scanner);
+			} else if (token.compareTo(".data") == 0) {
+				this.parseData(scanner);				
+			} else if (token.compareTo(".end") == 0) {
+			}
+		}
+	}
+
+	public void executeInstruction(Queue<Interrupt> interruptQueue) {
+		Instruction instruction = this.codeList.get(this.PC);
+		System.out.println(
+				instruction.getCommand()
+				+instruction.getOperand1()
+				+instruction.getOperand2());
+		this.PC = PC +1;
+		if (instruction.getOperand1().compareTo("halt") == 0) {
+			Interrupt interrupt =
+					new Interrupt(Interrupt.EInterrupt.eProcessTerminated, this);
+			interruptQueue.enqueue(interrupt);
+		}
+	}
+	
+	private class Instruction {
+		
+		private String command;
+		private String operand1;
+		private String operand2;
+		
+		public String getCommand() {
+			return command;
+		}
+		public String getOperand1() {
+			return operand1;
+		}
+		public void setOperand1(String operand1) {
+			this.operand1 = operand1;			
+		}
+		public String getOperand2() {
+			return operand2;
+		}
+		public Instruction(String line) {
+			String[] tokens = line.split(" ");
+			this.command = tokens[0];
+			this.operand1 = "";
+			this.operand2 = "";
+			
+			if (tokens.length > 1) {
+				this.operand1 = tokens[1];
+			}
+			if (tokens.length > 2) {
+				this.operand2 = tokens[2];
+			}
+		}
+	}
+}
