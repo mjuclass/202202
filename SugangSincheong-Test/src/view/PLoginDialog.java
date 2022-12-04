778ffeccddfdfea14a5c1d4f9c52d5bc0029ab62
Sungwoon Choi <choi.techwin@gmail.com>
2022-12-05 08:48:43 +0900

    1

--- SugangSincheong-Test/src/view/PLoginDialog.java
+++ SugangSincheong-Test/src/view/PLoginDialog.java
@@ -1,14 +1,15 @@
 package view;
 import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
@@ -33,46 +34,50 @@ public class PLoginDialog extends JDialog {
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		
 		
-		LayoutManager layoutManager = new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS);
+		LayoutManager layoutManager = new GridBagLayout();
 		this.setLayout(layoutManager);
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();
 		
-		JPanel panelHeader = new JPanel();
-		panelHeader.setSize(10,10);
-		this.add(panelHeader);
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
+		JLabel labelId = new JLabel(Constants.LoginDialog.LABEL_ID);
+		this.add(labelId, gridBagConstraints);
+		 		
+		gridBagConstraints.gridx = 1;
+		gridBagConstraints.gridy = 0;
+		gridBagConstraints.anchor = GridBagConstraints.CENTER;;
+		this.textFieldId = new JTextField();
+		this.textFieldId.setColumns(Constants.LoginDialog.SIZE_COLUMN_ID);
+		this.add(textFieldId, gridBagConstraints);		
+	
+		gridBagConstraints.insets = new Insets(10, 0, 0, 0);
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 1;
+		gridBagConstraints.anchor = GridBagConstraints.CENTER;
+		JLabel lbPassword =new JLabel(Constants.LoginDialog.LABEL_PASSWORD);    
+		this.add(lbPassword, gridBagConstraints);
 		
-		JPanel panelID = new JPanel();		
-			JLabel labelId = new JLabel(Constants.LoginDialog.LABEL_ID);
-			panelID.add(labelId);
-			 		
-			this.textFieldId = new JTextField();
-			this.textFieldId.setColumns(Constants.LoginDialog.SIZE_COLUMN_ID);
-			panelID.add(textFieldId);		
-		this.add(panelID);
+		gridBagConstraints.gridx = 1;
+		gridBagConstraints.gridy = 1;
+		this.passwordFieldPassword = new JPasswordField();   
+		this.passwordFieldPassword.setColumns(Constants.LoginDialog.SIZE_COLUMN_PASSWORD);
+		this.add(passwordFieldPassword,gridBagConstraints);
+	
+	
+		gridBagConstraints.insets = new Insets(20, 0, 0, 0);
+		gridBagConstraints.ipadx = 20;
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 2;
+		this.buttonOK = new JButton(Constants.LABEL_OK);    
+		this.buttonOK.addActionListener(actionHandler);
+		this.getRootPane().setDefaultButton(this.buttonOK);
+		this.add(this.buttonOK, gridBagConstraints);
 		
-		JPanel panelPassword = new JPanel();		
-			JLabel lbPassword =new JLabel(Constants.LoginDialog.LABEL_PASSWORD);    
-			panelPassword.add(lbPassword);
-			
-			this.passwordFieldPassword = new JPasswordField();   
-			this.passwordFieldPassword.setColumns(Constants.LoginDialog.SIZE_COLUMN_PASSWORD);
-			panelPassword.add(passwordFieldPassword);
-		this.add(panelPassword);
-		
-		
-		JPanel panelCommand = new JPanel();		
-			this.buttonOK = new JButton(Constants.LABEL_OK);    
-			this.buttonOK.addActionListener(actionHandler);
-			this.getRootPane().setDefaultButton(this.buttonOK);
-			panelCommand.add(this.buttonOK);
-			
-			this.buttonCancel = new JButton(Constants.LABEL_CANCEL);    
-			this.buttonCancel.addActionListener(actionHandler);
-			panelCommand.add(this.buttonCancel);			
-		this.add(panelCommand);
-		
-		JPanel paenlFooter = new JPanel();
-		paenlFooter.setSize(10,10);
-		this.add(paenlFooter);
+		gridBagConstraints.gridx = 1;
+		gridBagConstraints.gridy = 2;
+	this.buttonCancel = new JButton(Constants.LABEL_CANCEL);    
+		this.buttonCancel.addActionListener(actionHandler);
+		this.add(this.buttonCancel, gridBagConstraints);			
 
 		this.sLogin = new SLogin();
 	}
